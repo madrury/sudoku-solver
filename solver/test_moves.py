ae38152fdf6fda1137ba2baaaa9845cb7df99239
madrury <matthew.drury.83@gmail.com>
2017-07-13 20:16:11 -0400

    Add tests for HiddenSingle move.

--- solver/test_moves.py
+++ solver/test_moves.py
@@ -42,5 +42,52 @@ class TestNakedSingle(unittest.TestCase):
         self.assertEqual(ns, NakedSingle((0, 0), 1))
 
 
+class TestHiddenSingle(unittest.TestCase):
+
+    def test_hidden_single_row_top(self):
+        gb, mb = new_boards({
+            (1, 3): 1, (2, 6): 1, (0, 1): 2, (0, 2): 3
+        })
+        hs = HiddenSingle.search(mb)
+        self.assertEqual(hs, HiddenSingle((0, 0), 'row', 1))
+
+    def test_hidden_single_row_middle(self):
+        gb, mb = new_boards({
+            (3, 0): 1, (5, 6): 1, (4, 3): 2, (4, 5): 3
+        })
+        hs = HiddenSingle.search(mb)
+        self.assertEqual(hs, HiddenSingle((4, 4), 'row', 1))
+
+    def test_hidden_single_column_side(self):
+        gb, mb = new_boards({
+            (3, 1): 1, (6, 2): 1, (1, 0): 2, (2, 0): 3
+        })
+        hs = HiddenSingle.search(mb)
+        self.assertEqual(hs, HiddenSingle((0, 0), 'column', 1))
+
+    def test_hidden_single_column_middle(self):
+        gb, mb = new_boards({
+            (0, 3): 1, (6, 5): 1, (3, 4): 2, (5, 4): 3
+        })
+        hs = HiddenSingle.search(mb)
+        self.assertEqual(hs, HiddenSingle((4, 4), 'column', 1))
+
+
+    def test_hidden_single_box_top(self):
+        gb, mb = new_boards({
+            (1, 3): 1, (6, 1): 1, (2, 0): 2, (2, 2): 3, (0, 2): 4
+        })
+        hs = HiddenSingle.search(mb)
+        self.assertEqual(hs, HiddenSingle((0, 0), 'box', 1))
+
+    def test_hidden_single_box_middle(self):
+        gb, mb = new_boards({
+            (5, 0): 1, (0, 5): 1,
+            (4, 3): 2, (4, 4): 3, (3, 4): 4
+        })
+        hs = HiddenSingle.search(mb)
+        self.assertEqual(hs, HiddenSingle((3, 3), 'box', 1))
+
+
 if __name__ == '__main__':
     unittest.main()

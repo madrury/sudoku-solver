aed3eb81d9e2d4b05967b6ae737dcd55df2b589f
madrury <matthew.drury.83@gmail.com>
2017-08-06 19:23:10 -0700

    Add DifficultyVector.

--- solver/analysis.py
+++ solver/analysis.py
@@ -16,7 +16,7 @@ class MoveVector:
         self.solution = solution
     
     @staticmethod
-    def empty_solution_vector():
+    def empty_move_vector():
         return [0 for _ in MOVES_ORDER]
 
     @staticmethod
@@ -27,9 +27,26 @@ class MoveVector:
         return new_vector
 
     def __iter__(self):
-        solution_vector = MoveVector.empty_solution_vector()
+        solution_vector = MoveVector.empty_move_vector()
         yield solution_vector
         for move in self.solution.iter_moves():
             solution_vector = MoveVector.increment_from_move(
                 solution_vector, move)
             yield solution_vector
+
+
+class DifficultyVector:
+
+    def __init__(self, solution):
+        self.move_vector = MoveVector(solution)
+
+    def __iter__(self):
+        yield from (
+            1 + sum(sigmoid(move_count) for move_count in move_vector[2:]) 
+                    for move_vector in self.move_vector
+        )
+
+    def plot_difficulty_curve(self, ax, **kwargs):
+        difficulties = list(self)
+        n_moves = len(difficulties)
+        ax.plot(list(range(n_moves)), difficulties, **kwargs)

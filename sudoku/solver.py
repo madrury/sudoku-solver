fbd3253dfe3fdde1a0f2c7b6f055349706931358
Matthew Drury <matthew.drury.83@gmail.com>
2022-11-24 21:51:17 -0800

    Refactor Solver.

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -1,48 +1,16 @@
 import copy
 import json
+from typing import List, Set, Optional
 
-from sudoku.boards import MarkedBoard
-from sudoku.moves import MOVES_ORDER, MOVES_DICT, Finished
-
-
-class NotSolvableException(RuntimeError):
-    pass
-
-
-class Solver:
-    def __init__(self, game_board):
-        self.game_board = copy.deepcopy(game_board)
-        self.marked_board = MarkedBoard.from_game_board(game_board)
-        self.found_moves = set()
-        self.solution = Solution()
-
-    def find_next_move(self):
-        for move in MOVES_ORDER:
-            mv, new_marks = move.search(
-                self.marked_board, already_found=self.found_moves
-            )
-            if mv:
-                return mv, new_marks
-        raise NotSolvableException("Board is not solvable with current moveset")
-
-    def solve(self):
-        while True:
-            mv, marks = self.find_next_move()
-            if isinstance(mv, Finished):
-                self.solution.moves.append(mv)
-                return self.solution
-            else:
-                self.marked_board.add_marks(marks)
-                self.found_moves.add(mv)
-                self.solution.moves.append(mv)
-                self.solution.marks.append(marks)
-        return self.solution
+from sudoku.boards import GameBoard, MarkedBoard
+from sudoku.moves import MOVES_ORDER, MOVES_DICT, Finished, Move
 
 
 class Solution:
     def __init__(self):
-        self.moves = []
+        self.moves: List[Move] = []
         self.marks = []
+        self.is_full_solution = False
 
     def iter_moves(self):
         yield from self.moves
@@ -61,3 +29,41 @@ class Solution:
             move_name = move["name"]
             sln.moves.append(MOVES_DICT[move_name].from_dict(move))
         return sln
+
+
+class Solver:
+    def __init__(self, game_board: GameBoard):
+        self.game_board = copy.deepcopy(game_board)
+        self.marked_board = MarkedBoard.from_game_board(game_board)
+        self.found_moves: Set[Move] = set()
+        self.solution = Solution()
+        self.is_complete = False
+
+    def find_next_move(self) -> Optional[Move]:
+        for move in MOVES_ORDER:
+            mv = move.search(
+                self.marked_board, already_found=self.found_moves
+            )
+            if mv:
+                return mv
+        return None
+
+    def solve(self) -> Solution:
+        while not self.is_complete:
+            mv = self.find_next_move()
+            match mv:
+                case None:
+                    self.solution.is_fully_solved = False
+                    self.is_complete = True
+                case Finished():
+                    self.solution.moves.append(mv)
+                    self.solution.is_full_solution = True
+                    self.is_fully_solved = True
+                    self.is_complete = True
+                case _:
+                    marks = mv.compute_marks(self.marked_board)
+                    self.marked_board.add_marks(marks)
+                    self.found_moves.add(mv)
+                    self.solution.moves.append(mv)
+                    self.solution.marks.append(marks)
+        return self.solution
